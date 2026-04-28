71f2ad3c2cf58e530c9a921183e50570182f14af
灵亦 <yuecjn@gmail.com>
2026-04-28 20:55:36 +0800

    add Starops demo component and playground page for enhanced user experience
    Change-Id: If13ef77763b30e21b87c91826bbf48a193929823

---
+++ .vitepress/theme/src/components/STAROPSDemo.vue
@@ -0,0 +1,122 @@
+<script lang="ts" setup>
+import URI from 'urijs'
+import { computed, ref, watchEffect } from 'vue'
+import { initLang, isDarkTheme, parseCommonQuery } from './utils'
+import { inBrowser, useData } from 'vitepress'
+
+const props = defineProps(['workspace', 'region'])
+const workspace = props.workspace ?? 'default-cms-1819385687343877-cn-hongkong'
+const region = props.region ?? 'cn-hongkong'
+
+const { lang } = useData()
+watchEffect(() => {
+  if (inBrowser) {
+    initLang(lang.value)
+  }
+})
+
+const params = computed(() => {
+  const search = inBrowser ? window.location.search : ''
+  const queries = URI(search).query(true)
+
+  if (queries == null || queries.dest == null) {
+    return {
+      dest: `/`,
+      theme: 'default',
+      maxWidth: false,
+    }
+  }
+
+  return {
+    dest: queries.dest,
+    theme: isDarkTheme() ? 'dark' : 'default',
+    maxWidth: queries.maxWidth === true,
+  }
+})
+
+const { isShare } = parseCommonQuery()
+
+const tip = ref(
+  isShare
+    ? ''
+    : lang.value === 'en'
+    ? 'This is a demo environment. For production, please visit: '
+    : '当前为演示环境，StarOps 生产地址为：'
+)
+
+const productionUrl = 'https://starops.console.aliyun.com'
+
+let dest = ref('')
+
+watchEffect(async () => {
+  if (inBrowser) {
+    console.log(params.value.dest)
+    const target = params.value.dest.includes('?')
+      ? encodeURIComponent(params.value.dest)
+      : params.value.dest
+
+    const response = await fetch(
+      `https://cmsdemo-sls-demo-dussgynyta.cn-shanghai.fcapp.run?dest=${target}&target=starops`
+    )
+    const json = await response.json()
+    if (json.success) {
+      dest.value = json.data.url
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="container">
+    <iframe v-if="dest !== ''" :src="dest" :class="{ frame: true, 'max-width': params.maxWidth }">
+    </iframe>
+    <div class="tip" v-if="tip">
+      {{ tip }}<a :href="productionUrl" target="_blank">{{ productionUrl }}</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  height: calc(100vh - (var(--sls-topnav-height)));
+  width: 100vw;
+  position: relative;
+}
+
+.tip {
+  position: absolute;
+  top: 6px;
+  color: red;
+  width: 100%;
+  text-align: center;
+  padding: 10px 16px;
+  font-size: 18px;
+  pointer-events: none;
+}
+
+.tip a {
+  color: #1890ff;
+  text-decoration: underline;
+  pointer-events: auto;
+}
+
+.tip a:hover {
+  color: #40a9ff;
+}
+
+.frame {
+  height: calc(100vh - (var(--sls-topnav-height)));
+  width: 100vw;
+  border: none;
+  outline: none;
+  margin: auto;
+}
+
+.max-width {
+  max-width: var(--sls-page-max-width);
+}
+
+.dark .frame {
+  background-color: var(--vt-c-bg);
+}
+</style>
