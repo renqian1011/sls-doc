6f2ba0fad9f3d89fbbabe2c848849c66d5facb8c
灵亦 <linyi.wt@alibaba-inc.com>
2026-06-02 13:45:53 +0800

    fix(starops): update STAROps links to point to starops.html for consistency
    Change-Id: I7f8b1aecbec4539571adc1bf2e14c30ee2b1b5a6

--- .vitepress/theme/src/components/StaropsArticleTopbar.vue
+++ .vitepress/theme/src/components/StaropsArticleTopbar.vue
@@ -13,7 +13,7 @@ const isStaropsArticle = computed(() => pageClass.value === 'sls-starops-article
   <header v-if="isStaropsHome" class="sls-starops-topbar sls-starops-topbar--home">
     <div class="sls-starops-topbar__inner">
       <span class="sls-starops-topbar__brand">
-        <a class="sls-starops-topbar__brand-main" href="/doc/starops/index.html">
+        <a class="sls-starops-topbar__brand-main" href="/doc/starops/starops.html">
           <span class="sls-starops-topbar__mark"></span>
           <span>STAROps</span>
         </a>
@@ -21,7 +21,7 @@ const isStaropsArticle = computed(() => pageClass.value === 'sls-starops-article
         <a class="sls-starops-topbar__subbrand" href="/doc/index.html">阿里云可观测</a>
       </span>
       <nav class="sls-starops-topbar__links" aria-label="STAROps Navigation">
-        <a class="is-active" href="/doc/starops/index.html">最佳实践</a>
+        <a class="is-active" href="/doc/starops/starops.html">最佳实践</a>
         <a href="https://starops.console.aliyun.com" target="_blank" rel="noreferrer">控制台</a>
         <a class="sls-starops-topbar__cta sls-starops-topbar__cta--ghost" href="/doc/index.html">↗ 现有案例中心</a>
       </nav>
@@ -31,18 +31,18 @@ const isStaropsArticle = computed(() => pageClass.value === 'sls-starops-article
 
   <header v-else-if="isStaropsArticle" class="sls-starops-topbar sls-starops-topbar--article">
     <div class="sls-starops-topbar__inner">
-      <a class="sls-starops-topbar__brand" href="/doc/starops/index.html">
+      <a class="sls-starops-topbar__brand" href="/doc/starops/starops.html">
         <span class="sls-starops-topbar__mark"></span>
         <span>STAROps 实践</span>
       </a>
       <div class="sls-starops-topbar__crumb">
         <a href="/doc/index.html">案例中心</a>
         <span class="sep">/</span>
-        <a href="/doc/starops/index.html">STAROps</a>
+        <a href="/doc/starops/starops.html">STAROps</a>
         <span class="sep">/</span>
         <span>场景实践</span>
       </div>
-      <a class="sls-starops-topbar__cta" href="/doc/starops/index.html">← 返回 STAROps 子站点</a>
+      <a class="sls-starops-topbar__cta" href="/doc/starops/starops.html">← 返回 STAROps 子站点</a>
     </div>
   </header>
 </template>
